01682925990511fa02866b12b2503bc98472aed8
Cansancio21 <ryancansancio7@gmail.com>
2025-02-07 20:18:56 +0800

    third commit

--- index.php
+++ index.php
@@ -107,6 +107,22 @@
                     <input type="text" name="zip_code" placeholder="Zip Code" >
                 </div>
             </div>
+              <div class=country>
+              <label>country</label>
+            <select name="country" id="country" required>
+                <option value=""disabled selected>select</option>
+            <?php
+
+            $countries = [  "Albania", "Algeria", "American Samoa", "Andorra", "Angola", "Anguilla", "Antarctica", "Antigua and Barbuda", "Argentina", "Armenia","Aruba",            "Australia", "Austria", "Azerbaijan", "Bahamas (the)", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize",  "Benin", "Bermuda",            "Bhutan",            "Bolivia (Plurinational State of)",            "Bonaire, Sint Eustatius and Saba",            "Bosnia and Herzegovina",            "Botswana",            "Bouvet Island",            "Brazil",            "British Indian Ocean Territory (the)",            "Brunei Darussalam",            "Bulgaria",            "Burkina Faso",            "Burundi",            "Cabo Verde",            "Cambodia",            "Cameroon",            "Canada",            "Cayman Islands (the)",            "Central African Republic (the)",            "Chad",            "Chile",            "China",            "Christmas Island",            "Cocos (Keeling) Islands (the)",            "Colombia",            "Comoros (the)",            "Congo (the Democratic Republic of the)",            "Congo (the)",            "Cook Islands (the)",            "Costa Rica",            "Croatia",            "Cuba",            "Curaçao",            "Cyprus",            "Czechia",            "Côte d'Ivoire",            "Denmark",            "Djibouti",            "Dominica",            "Dominican Republic (the)",            "Ecuador",            "Egypt",            "El Salvador",            "Equatorial Guinea",            "Eritrea",            "Estonia",            "Eswatini",            "Ethiopia",            "Falkland Islands (the) [Malvinas]",            "Faroe Islands (the)",            "Fiji",            "Finland",            "France",            "French Guiana",            "French Polynesia",            "French Southern Territories (the)",            "Gabon",            "Gambia (the)",            "Georgia",            "Germany",            "Ghana",            "Gibraltar",            "Greece",            "Greenland",            "Grenada",            "Guadeloupe",            "Guam",            "Guatemala",            "Guernsey",            "Guinea",            "Guinea-Bissau",            "Guyana",            "Haiti",            "Heard Island and McDonald Islands",            "Holy See (the)",            "Honduras",            "Hong Kong",            "Hungary",            "Iceland",            "India",            "Indonesia",            "Iran (Islamic Republic of)",            "Iraq",            "Ireland",            "Isle of Man",            "Israel",            "Italy",            "Jamaica",            "Japan",            "Jersey",            "Jordan",            "Kazakhstan",            "Kenya",            "Kiribati",            "Korea (the Democratic People's Republic of)",            "Korea (the Republic of)",            "Kuwait",            "Kyrgyzstan",            "Lao People's Democratic Republic (the)",            "Latvia",            "Lebanon",            "Lesotho",            "Liberia",            "Libya",            "Liechtenstein",            "Lithuania",            "Luxembourg",            "Macao",            "Madagascar",            "Malawi",            "Malaysia",            "Maldives",            "Mali",            "Malta",            "Marshall Islands (the)",            "Martinique",            "Mauritania",            "Mauritius",            "Mayotte",            "Mexico",            "Micronesia (Federated States of)",            "Moldova (the Republic of)",            "Monaco",            "Mongolia",            "Montenegro",            "Montserrat",            "Morocco",            "Mozambique",            "Myanmar",            "Namibia",            "Nauru",            "Nepal",            "Netherlands (the)",            "New Caledonia",            "New Zealand",            "Nicaragua",            "Niger (the)",            "Nigeria",            "Niue",            "Norfolk Island",            "Northern Mariana Islands (the)",            "Norway",            "Oman",            "Pakistan",            "Palau",            "Palestine, State of",            "Panama",            "Papua New Guinea",            "Paraguay",            "Peru",            "Philippines (the)",            "Pitcairn",            "Poland",            "Portugal",            "Puerto Rico",            "Qatar",            "Republic of North Macedonia",            "Romania",            "Russian Federation (the)",            "Rwanda",            "Réunion",            "Saint Barthélemy",            "Saint Helena, Ascension and Tristan da Cunha",            "Saint Kitts and Nevis",            "Saint Lucia",            "Saint Martin (French part)",            "Saint Pierre and Miquelon",            "Saint Vincent and the Grenadines",            "Samoa",            "San Marino",            "Sao Tome and Principe",            "Saudi Arabia",            "Senegal",            "Serbia",            "Seychelles",            "Sierra Leone",            "Singapore",            "Sint Maarten (Dutch part)",            "Slovakia",            "Slovenia",            "Solomon Islands",            "Somalia",            "South Africa",            "South Georgia and the South Sandwich Islands",            "South Sudan",            "Spain",            "Sri Lanka",            "Sudan (the)",            "Suriname",            "Svalbard and Jan Mayen",            "Sweden",            "Switzerland",            "Syrian Arab Republic",            "Taiwan",            "Tajikistan",            "Tanzania, United Republic of",            "Thailand",            "Timor-Leste",            "Togo",            "Tokelau",            "Tonga",            "Trinidad and Tobago",            "Tunisia",            "Turkey",            "Turkmenistan",            "Turks and Caicos Islands (the)",            "Tuvalu",            "Uganda",            "Ukraine",            "United Arab Emirates (the)",            "United Kingdom of Great Britain and Northern Ireland (the)",            "United States Minor Outlying Islands (the)",            "United States of America (the)",            "Uruguay",            "Uzbekistan",            "Vanuatu",            "Venezuela (Bolivarian Republic of)",            "Viet Nam",            "Virgin Islands (British)",            "Virgin Islands (U.S.)",            "Wallis and Futuna",            "Western Sahara",            "Yemen",            "Zambia",            "Zimbabwe",            "Åland Islands"
+        ];
+
+        foreach($countries as $country){
+            echo"<option value=\"$country\">$country</option>";
+        }
+        ?>
+        </select>
+              </div>
+            
 
             <h2>Home Address</h2>
             <div class="place">
@@ -146,10 +162,31 @@
                     <input type="text" name="zip_code" placeholder="Zip Code"  required>
                 </div>
 
-            </div> <div class="number">
+                
+
+            </div>
+            
+            <div class=country>
+              <label>country</label>
+            <select name="country" id="country" required>
+                <option value=""disabled selected>select</option>
+            <?php
+
+            $countries = [  "Albania", "Algeria", "American Samoa", "Andorra", "Angola", "Anguilla", "Antarctica", "Antigua and Barbuda", "Argentina", "Armenia","Aruba",            "Australia", "Austria", "Azerbaijan", "Bahamas (the)", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize",  "Benin", "Bermuda",            "Bhutan",            "Bolivia (Plurinational State of)",            "Bonaire, Sint Eustatius and Saba",            "Bosnia and Herzegovina",            "Botswana",            "Bouvet Island",            "Brazil",            "British Indian Ocean Territory (the)",            "Brunei Darussalam",            "Bulgaria",            "Burkina Faso",            "Burundi",            "Cabo Verde",            "Cambodia",            "Cameroon",            "Canada",            "Cayman Islands (the)",            "Central African Republic (the)",            "Chad",            "Chile",            "China",            "Christmas Island",            "Cocos (Keeling) Islands (the)",            "Colombia",            "Comoros (the)",            "Congo (the Democratic Republic of the)",            "Congo (the)",            "Cook Islands (the)",            "Costa Rica",            "Croatia",            "Cuba",            "Curaçao",            "Cyprus",            "Czechia",            "Côte d'Ivoire",            "Denmark",            "Djibouti",            "Dominica",            "Dominican Republic (the)",            "Ecuador",            "Egypt",            "El Salvador",            "Equatorial Guinea",            "Eritrea",            "Estonia",            "Eswatini",            "Ethiopia",            "Falkland Islands (the) [Malvinas]",            "Faroe Islands (the)",            "Fiji",            "Finland",            "France",            "French Guiana",            "French Polynesia",            "French Southern Territories (the)",            "Gabon",            "Gambia (the)",            "Georgia",            "Germany",            "Ghana",            "Gibraltar",            "Greece",            "Greenland",            "Grenada",            "Guadeloupe",            "Guam",            "Guatemala",            "Guernsey",            "Guinea",            "Guinea-Bissau",            "Guyana",            "Haiti",            "Heard Island and McDonald Islands",            "Holy See (the)",            "Honduras",            "Hong Kong",            "Hungary",            "Iceland",            "India",            "Indonesia",            "Iran (Islamic Republic of)",            "Iraq",            "Ireland",            "Isle of Man",            "Israel",            "Italy",            "Jamaica",            "Japan",            "Jersey",            "Jordan",            "Kazakhstan",            "Kenya",            "Kiribati",            "Korea (the Democratic People's Republic of)",            "Korea (the Republic of)",            "Kuwait",            "Kyrgyzstan",            "Lao People's Democratic Republic (the)",            "Latvia",            "Lebanon",            "Lesotho",            "Liberia",            "Libya",            "Liechtenstein",            "Lithuania",            "Luxembourg",            "Macao",            "Madagascar",            "Malawi",            "Malaysia",            "Maldives",            "Mali",            "Malta",            "Marshall Islands (the)",            "Martinique",            "Mauritania",            "Mauritius",            "Mayotte",            "Mexico",            "Micronesia (Federated States of)",            "Moldova (the Republic of)",            "Monaco",            "Mongolia",            "Montenegro",            "Montserrat",            "Morocco",            "Mozambique",            "Myanmar",            "Namibia",            "Nauru",            "Nepal",            "Netherlands (the)",            "New Caledonia",            "New Zealand",            "Nicaragua",            "Niger (the)",            "Nigeria",            "Niue",            "Norfolk Island",            "Northern Mariana Islands (the)",            "Norway",            "Oman",            "Pakistan",            "Palau",            "Palestine, State of",            "Panama",            "Papua New Guinea",            "Paraguay",            "Peru",            "Philippines (the)",            "Pitcairn",            "Poland",            "Portugal",            "Puerto Rico",            "Qatar",            "Republic of North Macedonia",            "Romania",            "Russian Federation (the)",            "Rwanda",            "Réunion",            "Saint Barthélemy",            "Saint Helena, Ascension and Tristan da Cunha",            "Saint Kitts and Nevis",            "Saint Lucia",            "Saint Martin (French part)",            "Saint Pierre and Miquelon",            "Saint Vincent and the Grenadines",            "Samoa",            "San Marino",            "Sao Tome and Principe",            "Saudi Arabia",            "Senegal",            "Serbia",            "Seychelles",            "Sierra Leone",            "Singapore",            "Sint Maarten (Dutch part)",            "Slovakia",            "Slovenia",            "Solomon Islands",            "Somalia",            "South Africa",            "South Georgia and the South Sandwich Islands",            "South Sudan",            "Spain",            "Sri Lanka",            "Sudan (the)",            "Suriname",            "Svalbard and Jan Mayen",            "Sweden",            "Switzerland",            "Syrian Arab Republic",            "Taiwan",            "Tajikistan",            "Tanzania, United Republic of",            "Thailand",            "Timor-Leste",            "Togo",            "Tokelau",            "Tonga",            "Trinidad and Tobago",            "Tunisia",            "Turkey",            "Turkmenistan",            "Turks and Caicos Islands (the)",            "Tuvalu",            "Uganda",            "Ukraine",            "United Arab Emirates (the)",            "United Kingdom of Great Britain and Northern Ireland (the)",            "United States Minor Outlying Islands (the)",            "United States of America (the)",            "Uruguay",            "Uzbekistan",            "Vanuatu",            "Venezuela (Bolivarian Republic of)",            "Viet Nam",            "Virgin Islands (British)",            "Virgin Islands (U.S.)",            "Wallis and Futuna",            "Western Sahara",            "Yemen",            "Zambia",            "Zimbabwe",            "Åland Islands"
+        ];
+
+        foreach($countries as $country){
+            echo"<option value=\"$country\">$country</option>";
+        }
+        ?>
+        </select>
+              </div>
+            
+            
+            <div class="number">
             <div class="input-number">
              <label>Mobile/Cellphone Number</label>
-             <input type="text" name="tax_id" placeholder="Enter Mobile Number" required pattern="[0-9]+" title="Must accept Numbers Only">
+             <input type="text" name="Mobile_id" placeholder="Enter Mobile Number" required pattern="[0-9]+" title="Must accept Numbers Only">
             </div>
             <div class="input-number">
     <label>E-mail Address</label>
@@ -157,7 +194,7 @@
        </div>
            <div class="input-number">
     <label>Telephone Number</label>
-    <input type="text" name="tax_id" placeholder="Enter Telephone Number" required pattern="[0-9]+" title="Must accept Numbers Only">
+    <input type="text" name=telephone_id" placeholder="Enter Telephone Number" required pattern="[0-9]+" title="Must accept Numbers Only">
          </div>
             </div>
 
@@ -196,10 +233,7 @@
             </div>
 
             <div class="buttons">
-                <button class="backBtn">
-                    <span class="btnText">Back</span>
-                </button>
-
+               
                 <button type="submit" class="subBtn">
                     <span class="btnText">Submit</span>
                 </button>
--- script.js
+++ script.js
@@ -29,3 +29,6 @@ const form = document.querySelector("form"),
             }
         });
     
+
+
+        
